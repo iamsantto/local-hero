46b4e704d4820a9b45b08ebf601f61cc7bb1c65e
Santhosh <santhosh@keepworks.com>
2018-02-12 20:00:01 +0530

    Added routes, modular css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { MemoryRouter } from 'react-router'
+
+import App from './components/App'
+
+render((
+  <MemoryRouter initialEntries={['/home', '/options']} initialIndex={0}>
+    <App />
+  </MemoryRouter>
+), document.getElementById('root'))
